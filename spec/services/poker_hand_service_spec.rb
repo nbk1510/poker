9807c8f6d436d13ec6293a445c4422c55705ea44
nbk1510 <nbk1510@gmail.com>
2024-12-06 11:59:01 +0700

    Refactor spec to use shared_examples

--- spec/services/poker_hand_service_spec.rb
+++ spec/services/poker_hand_service_spec.rb
@@ -25,85 +25,47 @@ RSpec.describe PokerHandService do
     end
 
     context 'Check business logic' do
-      context 'Straight flush hand' do
-        let(:hands_data) { ['C7 C6 C5 C4 C3'] }
-
-        it 'return correct hand result' do
+      shared_examples 'returns correct poker hand' do |hands_data:, expected_hand:|
+        it do
           output = PokerHandService::Analyzer.new(hands_data).analyze
-          expect(output['result'].first['hand']).to eq 'Straight flush'
+          expect(output['result'].first['hand']).to eq expected_hand
         end
+      end
+
+      context 'Straight flush hand' do
+        it_behaves_like 'returns correct poker hand', hands_data: ['C7 C6 C5 C4 C3'], expected_hand: 'Straight flush'
       end
 
       context 'Four of a kind hand' do
-        let(:hands_data) { ['C10 D10 H10 S10 D5'] }
-
-        it 'return correct hand result' do
-          output = PokerHandService::Analyzer.new(hands_data).analyze
-          expect(output['result'].first['hand']).to eq 'Four of a kind'
-        end
+        it_behaves_like 'returns correct poker hand', hands_data: ['C10 D10 H10 S10 D5'], expected_hand: 'Four of a kind'
       end
 
       context 'Full house hand' do
-        let(:hands_data) { ['S10 H10 D10 S4 D4'] }
-
-        it 'return correct hand result' do
-          output = PokerHandService::Analyzer.new(hands_data).analyze
-          expect(output['result'].first['hand']).to eq 'Full house'
-        end
+        it_behaves_like 'returns correct poker hand', hands_data: ['S10 H10 D10 S4 D4'], expected_hand: 'Full house'
       end
 
       context 'Flush hand' do
-        let(:hands_data) { ['H1 H12 H10 H5 H3'] }
-
-        it 'return correct hand result' do
-          output = PokerHandService::Analyzer.new(hands_data).analyze
-          expect(output['result'].first['hand']).to eq 'Flush'
-        end
+        it_behaves_like 'returns correct poker hand', hands_data: ['H1 H12 H10 H5 H3'], expected_hand: 'Flush'
       end
 
       context 'Straight hand' do
-        let(:hands_data) { ['S8 S7 H6 H5 S4'] }
-
-        it 'return correct hand result' do
-          output = PokerHandService::Analyzer.new(hands_data).analyze
-          expect(output['result'].first['hand']).to eq 'Straight'
-        end
+        it_behaves_like 'returns correct poker hand', hands_data: ['S8 S7 H6 H5 S4'], expected_hand: 'Straight'
       end
 
       context 'Three of a kind hand' do
-        let(:hands_data) { ['S12 C12 D12 S5 C3'] }
-
-        it 'return correct hand result' do
-          output = PokerHandService::Analyzer.new(hands_data).analyze
-          expect(output['result'].first['hand']).to eq 'Three of a kind'
-        end
+        it_behaves_like 'returns correct poker hand', hands_data: ['S12 C12 D12 S5 C3'], expected_hand: 'Three of a kind'
       end
 
       context 'Two pair hand' do
-        let(:hands_data) { ['H13 D13 C2 D2 H11'] }
-
-        it 'return correct hand result' do
-          output = PokerHandService::Analyzer.new(hands_data).analyze
-          expect(output['result'].first['hand']).to eq 'Two pair'
-        end
+        it_behaves_like 'returns correct poker hand', hands_data: ['H13 D13 C2 D2 H11'], expected_hand: 'Two pair'
       end
 
       context 'One pair hand' do
-        let(:hands_data) { ['C10 S10 S6 H4 H2'] }
-
-        it 'return correct hand result' do
-          output = PokerHandService::Analyzer.new(hands_data).analyze
-          expect(output['result'].first['hand']).to eq 'One pair'
-        end
+        it_behaves_like 'returns correct poker hand', hands_data: ['C10 S10 S6 H4 H2'], expected_hand: 'One pair'
       end
 
       context 'High card hand' do
-        let(:hands_data) { ['D1 D10 S9 C5 C4'] }
-
-        it 'return correct hand result' do
-          output = PokerHandService::Analyzer.new(hands_data).analyze
-          expect(output['result'].first['hand']).to eq 'High card'
-        end
+        it_behaves_like 'returns correct poker hand', hands_data: ['D1 D10 S9 C5 C4'], expected_hand: 'High card'
       end
     end
 
